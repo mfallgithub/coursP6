69039b81cf3105cf14de61ec053bcf905b1046eb
Moustapha FALL <moustapha.fall.external@worldline.com>
2022-07-18 18:13:33 +0000

    implementer d'un model thing avec l'api dynamique

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //importer express
 const express = require('express');
 const app =express();
-
+const bodyParser =require('body-parser');
 const mongoose = require('mongoose');
-
+const Thing = require('./models/Thing');
 mongoose.connect('mongodb+srv://mfalluser:iymzagp0MWVnLxVn@atlascluster.4dm1h.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -28,8 +28,8 @@ app.use((req,res) =>{
    console.log('Réponse envoyée avec succès !');
 });*/
 
-app.use(express.json());
-
+//app.use(express.json());
+app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -37,32 +37,24 @@ app.use((req, res, next) => {
     next();
   });
 app.post('/api/stuff',(req,res,next) =>{
-    console.log(req.body);
-    res.status(201).json({
-      message: 'Objet crée !'
+  delete req.body._id;
+    const thing = new Thing({
+       ...req.body
     });
+    thing.save()
+      .then(() =>res.status(201).json({message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({error}));
 });
   
+app.get('/api/stuff/:id', (req,res, next) =>{
+   Thing.findOne({_id: req.params.id})
+   .then(thing => res.status(200).json(thing))
+   .catch(error => res.status(404).json({error}));
+});
 app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
+    Thing.find()
+      .then(things => res.status(200).json(things))
+      .catch(error => res.status(400).json({error}));
   });
 
 module.exports = app;
