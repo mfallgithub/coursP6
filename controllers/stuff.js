d4d31e1602e0d6299e1cb7b21fc56f740aa6aa22
Moustapha FALL <moustapha.fall.external@worldline.com>
2022-07-28 16:05:33 +0000

    Add a delete method with multer

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,4 +1,6 @@
 const Thing = require('../models/Thing');
+//import fs
+const fs = require('fs');
 
 //without using multer
 /*exports.createThing = (req, res, next) =>  {
@@ -50,10 +52,30 @@ exports.modifyThing = (req, res, next) => {
             }
         }).catch((error) => { res.status(400).json({ error }) });
 };
-exports.deleteThing = (req, res, next) => {
+//without multer
+/*exports.deleteThing = (req, res, next) => {
     Thing.deleteOne({ _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
         .catch(error => res.status(400).json({ error }));
+};*/
+//with multer
+exports.deleteThing = (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then(thing => {
+            if (thing.userId != req.auth.userId) {
+                res.status(401).json({ message: 'Not authorized' });
+            } else {
+                const filename = thing.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    Thing.deleteOne({ _id: req.params.id })
+                        .then(() => { res.status(200).json({ message: 'Objet supprimé !' }) })
+                        .catch(error => res.status(401).json({ error }));
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error });
+        });
 };
 
 exports.getOneThing = (req, res, next) => {
@@ -61,6 +83,7 @@ exports.getOneThing = (req, res, next) => {
         .then(thing => res.status(200).json(thing))
         .catch(error => res.status(404).json({ error }));
 };
+
 exports.getAllThings = (req, res, next) => {
     Thing.find()
         .then(things => res.status(200).json(things))
